3b91f4f3e2851176efbb369647482873ee4aa3ad
rahulXs <rahulsharmaacn@gmail.com>
2018-07-10 22:13:24 +0530

    Initial commit

---
+++ script.py
@@ -0,0 +1,26 @@
+import cv2
+from datetime import datetime
+
+first_frame=None
+video=cv2.VideoCapture(0)
+
+while True:
+    check, frame= video.read()
+
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray, (21,21),0)
+
+    if first_frame is None:
+        first_frame=gray
+        continue
+
+    delta_frame=cv2.absdiff(first_frame, gray)
+    cv2.imshow("Gray frame", gray)
+    cv2.imshow("Delta frame", delta_frame)
+
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break
+
+video.release
+cv2.destroyAllWindows
