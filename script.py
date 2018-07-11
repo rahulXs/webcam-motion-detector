f7e434fcc405a0cfa3b5e3b986689371ed3f6ef9
rahulXs <rahulsharmaacn@gmail.com>
2018-07-11 23:32:07 +0530

    Add recording time

--- script.py
+++ script.py
@@ -1,12 +1,16 @@
-import cv2
+import cv2, time, pandas
 from datetime import datetime
 
 first_frame=None
+status_list=[None,None]
+times=[]
+df=pandas.DataFrame(columns=["Start","End"])
+
 video=cv2.VideoCapture(0)
 
 while True:
     check, frame= video.read()
-
+    status = 0
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray, (21,21),0)
 
@@ -24,8 +28,17 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour)<1000:
             continue
+        status = 1
         (x,y,w,h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),3)
+    status_list.append(status)
+
+    status_list = status_list[-2:]
+
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
 
     cv2.imshow("Gray frame", gray)
     cv2.imshow("Delta frame", delta_frame)
@@ -34,7 +47,13 @@ while True:
 
     key=cv2.waitKey(1)
     if key==ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
 
+for i in range(0,len(times),2):
+    df=df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+
+df.to_csv("Times.csv")
 video.release
 cv2.destroyAllWindows
