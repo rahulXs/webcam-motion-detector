2a7914521fe23a6a18d0f3fc6a02e597a8f80cc5
rahulXs <rahulsharmaacn@gmail.com>
2018-07-11 23:28:23 +0530

    Add contours

--- script.py
+++ script.py
@@ -17,9 +17,20 @@ while True:
     delta_frame=cv2.absdiff(first_frame, gray)
     thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+
+
+    (__,cnts,__)=cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour)<1000:
+            continue
+        (x,y,w,h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0),3)
+
     cv2.imshow("Gray frame", gray)
     cv2.imshow("Delta frame", delta_frame)
     cv2.imshow("Thresh frame", thresh_frame)
+    cv2.imshow("Color frame", frame)
 
     key=cv2.waitKey(1)
     if key==ord('q'):
